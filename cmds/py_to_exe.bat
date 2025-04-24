0cffa5210a3461eea3953fec54d1374d74b1ec0e
kexian.kuo <YOUNGOPTICS>
2025-04-24 10:46:30 +0800

    feat: 更新批次檔以移除不必要的命令和註解
    在 py_to_exe.bat 和 run-server.bat 中，移除不必要的命令和註解，並添加說明以提高可讀性，確保使用者能更清楚地理解批次檔的功能。

--- cmds/py_to_exe.bat
+++ cmds/py_to_exe.bat
@@ -1,15 +1,17 @@
-@ECHO OFF
+@REM Debugpy 套件不好編譯成exe，所以不使用
 
-@REM 設定命令提示字元編碼為UTF-8，以正確顯示中文
-chcp 65001
+@REM @ECHO OFF
 
-@REM 切換到目前批次檔所在目錄的上一層目錄
-cd /d %~dp0\..
+@REM @REM 設定命令提示字元編碼為UTF-8，以正確顯示中文
+@REM chcp 65001
 
-@REM 使用pyinstaller編譯
-pyinstaller ^
---onefile ^
---name=debugpy-server ^
-run-server.py
+@REM @REM 切換到目前批次檔所在目錄的上一層目錄
+@REM cd /d %~dp0\..
 
-MSG * 編譯完成，請按任意鍵結束
+@REM @REM 使用pyinstaller編譯
+@REM pyinstaller ^
+@REM --onefile ^
+@REM --name=debugpy-server ^
+@REM run-server.py
+
+@REM MSG * 編譯完成，請按任意鍵結束
--- cmds/run-server.bat
+++ cmds/run-server.bat
@@ -1,4 +1,6 @@
-@REM 切換到目前批次檔所在目錄的上一層目錄
-cd /d %~dp0\..
+@REM 只能用於本地端 debugpy 連線，不好用
 
-python -m debugpy --listen 10.3.63.15:5678 --wait-for-client ./main.ipynb
+@REM @REM 切換到目前批次檔所在目錄的上一層目錄
+@REM cd /d %~dp0\..
+
+@REM python -m debugpy --listen 10.3.63.15:5678 --wait-for-client ./main.ipynb
